8d045409ce73e165b98f355cd0f494a036839811
handriss <andrashinkel@gmail.com>
2017-03-07 20:35:07 +0100

    Role class decorated

--- src/main/java/com/codecool/domain/Role.java
+++ src/main/java/com/codecool/domain/Role.java
@@ -2,18 +2,26 @@ package com.codecool.domain;
 
 
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 import java.util.Set;
 
 @Data
 @Entity
+@Table(name="role")
+@Slf4j
 public class Role {
 
+    @Id
+    @GeneratedValue
+    @Column(name="role_id", unique = true)
     private Long id;
 
+    @Column(name="email")
     private String name;
 
+    @ManyToMany(mappedBy = "roles")
     private Set<User> users;
 
     private Role(){}
