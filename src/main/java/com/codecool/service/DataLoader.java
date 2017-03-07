2b9ae666c5008e175f86daa637a2ca5e1591afe2
handriss <andrashinkel@gmail.com>
2017-03-07 21:01:44 +0100

    example instances created in dataloader

--- src/main/java/com/codecool/service/DataLoader.java
+++ src/main/java/com/codecool/service/DataLoader.java
@@ -1,7 +1,43 @@
 package com.codecool.service;
 
-/**
- * Created by handris on 3/7/17.
- */
+
+import com.codecool.domain.Role;
+import com.codecool.domain.User;
+import com.codecool.repository.RoleRepository;
+import com.codecool.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Slf4j
 public class DataLoader {
+
+    private UserService userService;
+
+    private RoleService roleService;
+
+    @Autowired
+    public DataLoader(UserService userService, RoleService roleService) {
+        this.userService = userService;
+        this.roleService = roleService;
+    }
+
+
+
+    @PostConstruct
+    public void loadData(){
+
+        Set<Role> roles = new HashSet<>();
+        roles.add(new Role("ADMIN"));
+        roles.add(new Role("USER"));
+
+        User user = new User("andrashinkel@gmail.com", "admin", roles);
+
+        user
+    }
 }
--- src/main/java/com/codecool/service/UserService.java
+++ src/main/java/com/codecool/service/UserService.java
@@ -1,7 +1,5 @@
 package com.codecool.service;
 
-/**
- * Created by handris on 3/7/17.
- */
+
 public class UserService {
 }
