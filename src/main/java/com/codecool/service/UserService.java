b6433a5dec7f01c4dcce6a8336cb9188916e4df4
handriss <andrashinkel@gmail.com>
2017-03-10 15:28:55 +0100

    UserDetailsImplementation done

--- src/main/java/com/codecool/service/UserService.java
+++ src/main/java/com/codecool/service/UserService.java
@@ -4,13 +4,16 @@ package com.codecool.service;
 import com.codecool.domain.User;
 import com.codecool.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Slf4j
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     private UserRepository userRepository;
 
@@ -36,5 +39,15 @@ public class UserService {
 
     public User findByEmail(String email) {
         return userRepository.findByEmail(email);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        User user = findByEmail(userName);
+        if(user == null){
+            throw new UsernameNotFoundException(userName);
+        }
+
+        return new UserDetailsImpl(user);
     }
 }
