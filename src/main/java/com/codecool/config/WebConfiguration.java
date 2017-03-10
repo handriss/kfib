678f18cc590ba8c854dc6e1375cb8a78f41bf8da
handriss <andrashinkel@gmail.com>
2017-03-10 12:25:19 +0100

    webconfiguration implemented

--- src/main/java/com/codecool/config/WebConfiguration.java
+++ src/main/java/com/codecool/config/WebConfiguration.java
@@ -1,5 +1,15 @@
 package com.codecool.config;
 
-public class WebConfiguration {
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+public class WebConfiguration extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry viewControllerRegistry) {
+        super.addViewControllers(viewControllerRegistry);
+        viewControllerRegistry.addViewController("/login").setViewName("auth/login");
+        viewControllerRegistry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    }
 }
