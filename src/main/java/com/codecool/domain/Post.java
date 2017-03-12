c764f7c5e821e026c2f191c3e7c81134dc868484
handriss <andrashinkel@gmail.com>
2017-03-12 17:45:42 +0100

    Post annotated to jpa

--- src/main/java/com/codecool/domain/Post.java
+++ src/main/java/com/codecool/domain/Post.java
@@ -4,20 +4,41 @@ package com.codecool.domain;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
 
+import javax.persistence.*;
 import java.util.Date;
 
 @Getter
 @Setter
 @ToString
+@Entity
+@Table(name="post")
 public class Post {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
+
+    @Column(columnDefinition = "TEXT")
     private String body;
+
+    @Column(columnDefinition = "TEXT")
     private String teaser;
+
     private String slug;
+
+    @CreatedDate
+    @Temporal(TemporalType.TIMESTAMP)
     private Date postedOn;
+
+    @ManyToOne
     private Author author;
 
+    private Post(){}
+
+    public Post(String title) {
+        this.title = title;
+    }
 }
