55e1855465c52d470b4087a08d1086cf1b9a9b69
handriss <andrashinkel@gmail.com>
2017-03-12 16:26:45 +0100

    swagger added

--- src/main/java/com/codecool/config/SecurityConfiguration.java
+++ src/main/java/com/codecool/config/SecurityConfiguration.java
@@ -31,7 +31,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.
                 authorizeRequests()
-                    .antMatchers("/").permitAll()
+                    .antMatchers("/", "/swagger-ui.html", "/v2/api-docs", "/configuration/ui", "/swagger-resources").permitAll()
                     .antMatchers("/admin/**").hasRole("ADMIN")
                     .anyRequest().authenticated()
                 .and()
