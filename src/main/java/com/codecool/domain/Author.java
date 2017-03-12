fde9f914f1c0389bb2f6615361bdeb790ee4c7ff
handriss <andrashinkel@gmail.com>
2017-03-12 17:51:10 +0100

    Author class annotated to jpa

--- src/main/java/com/codecool/domain/Author.java
+++ src/main/java/com/codecool/domain/Author.java
@@ -4,13 +4,25 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.persistence.*;
+
 @Getter
 @Setter
 @ToString
+@Entity
+@Table(name="author")
 public class Author {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
 
+    private Author(){}
+
+    public Author(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
 }
