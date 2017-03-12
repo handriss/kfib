b38372ee10fd35b66aa47d9c9acf3e50eb1bca64
handriss <andrashinkel@gmail.com>
2017-03-12 17:34:17 +0100

    Post pojo created

---
+++ src/main/java/com/codecool/domain/Post.java
@@ -0,0 +1,17 @@
+package com.codecool.domain;
+
+
+import java.util.Date;
+
+public class Post {
+
+    private Long id;
+    private String title;
+    private String body;
+    private String teaser;
+    private String slug;
+    private Date postedOn;
+    private Author author;
+
+
+}
