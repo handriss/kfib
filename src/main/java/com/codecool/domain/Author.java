b2e215638e1af1c871fe9ce21e171345f7c90aea
handriss <andrashinkel@gmail.com>
2017-03-12 17:37:21 +0100

    setter, getter, tostring added to post and author

--- src/main/java/com/codecool/domain/Author.java
+++ src/main/java/com/codecool/domain/Author.java
@@ -1,6 +1,12 @@
 package com.codecool.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@ToString
 public class Author {
 
     private Long id;
--- src/main/java/com/codecool/domain/Post.java
+++ src/main/java/com/codecool/domain/Post.java
@@ -1,8 +1,15 @@
 package com.codecool.domain;
 
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 import java.util.Date;
 
+@Getter
+@Setter
+@ToString
 public class Post {
 
     private Long id;
