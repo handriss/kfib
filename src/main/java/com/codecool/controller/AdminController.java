f56202cfd12f42b530870a5ba34271d968121882
handriss <andrashinkel@gmail.com>
2017-03-14 01:17:01 +0100

    ich bin uberrascht dass es funktioniert

--- src/main/java/com/codecool/controller/AdminController.java
+++ src/main/java/com/codecool/controller/AdminController.java
@@ -3,12 +3,14 @@ package com.codecool.controller;
 
 import com.codecool.service.PostService;
 import com.codecool.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@Slf4j
 public class AdminController {
 
     private PostService postService;
@@ -23,6 +25,7 @@ public class AdminController {
     @RequestMapping("/admin/posts")
     public String list(Model model){
         model.addAttribute("posts", postService.findAll());
+        log.info(postService.findAll().toString());
         return "admin/list";
     }
 
