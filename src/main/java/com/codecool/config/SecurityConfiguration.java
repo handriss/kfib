394870f44964fa3aea6b0cb4d67389eb9bf2985f
handriss <andrashinkel@gmail.com>
2017-03-15 22:29:45 +0100

    securityconfig now working with 403 and 404 errors

--- src/main/java/com/codecool/config/SecurityConfiguration.java
+++ src/main/java/com/codecool/config/SecurityConfiguration.java
@@ -9,8 +9,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Slf4j
@@ -25,38 +23,27 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
-    /* Override the configure method and permit all user to see the login and registration page
-    and to log out. */
+
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.
-                authorizeRequests()
-                    .antMatchers("/admin/posts", "/admin/create-post","/", "/assets/**", "/swagger-ui.html", "/v2/api-docs", "/configuration/ui", "/swagger-resources").permitAll()
-//                    .antMatchers("/admin/**").hasRole("ADMIN")
-                    .anyRequest().authenticated()
-                .and()
-                    .formLogin()
-//                    .successHandler(successHandler())
-                    .loginPage("/login")
-                    .permitAll()
-                .and()
-                    .logout()
-                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                    .logoutSuccessUrl("/login?logout")
-                    .permitAll();
+        http.authorizeRequests()
+                .antMatchers("/admin/posts", "/admin/create-post","/", "/assets/**", "/swagger-ui.html", "/v2/api-docs", "/configuration/ui", "/swagger-resources").permitAll()
+                .antMatchers("/authors").hasRole("ADMIN")
+            .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+            .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login?logout")
+                .permitAll();
 
-//        http.formLogin().defaultSuccessUrl("/admin", true);
     }
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
-    }
-
-    @Bean
-    public AuthenticationSuccessHandler successHandler() {
-        SimpleUrlAuthenticationSuccessHandler handler = new SimpleUrlAuthenticationSuccessHandler();
-        handler.setUseReferer(false);
-        return handler;
     }
 }
