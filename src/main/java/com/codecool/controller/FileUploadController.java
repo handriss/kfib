cbf1b6637b9fcc1a776724ce3b39beeee2a4e0e5
handriss <andrashinkel@gmail.com>
2017-03-15 22:52:17 +0100

    storageexception done

---
+++ src/main/java/com/codecool/controller/FileUploadController.java
@@ -0,0 +1,7 @@
+package com.codecool.controller;
+
+/**
+ * Created by handris on 3/15/17.
+ */
+public class FileUploadController {
+}
---
+++ src/main/java/com/codecool/service/upload/FileSystemStorageService.java
@@ -0,0 +1,7 @@
+package com.codecool.service.upload;
+
+/**
+ * Created by handris on 3/15/17.
+ */
+public class FileSystemStorageService {
+}
