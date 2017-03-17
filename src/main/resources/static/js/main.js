740b652127a82335a49fa729b4a579d211d3420a
handriss <andrashinkel@gmail.com>
2017-03-17 21:48:57 +0100

    datatables translated into my favourite Ugric language

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,6 +1,22 @@
 $(document).ready(function() {
-    $('#post-list').DataTable();
-    $('#file-list').DataTable();
+
+    var hungarian = {
+        "lengthMenu": "Oldalméret: _MENU_",
+        "zeroRecords": "A táblázatban nincsenek adatok",
+        "info": "_PAGE_. oldal a(z) _PAGES_ oldalból",
+        "infoEmpty": "A táblázatban nincsenek adatok",
+        "infoFiltered": "(_MAX_ elem közül kiválasztva)",
+        "sSearch": "Keresés:",
+        oPaginate: {sFirst: "Első", sLast: "Utolsó", sNext: "Következő", sPrevious: "Előző"}
+    };
+
+    $('#post-list').DataTable({
+        "language": hungarian
+    });
+
+    $('#file-list').DataTable({
+        "language": hungarian
+    });
+
+
 } );
-
-
