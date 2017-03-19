2108eb105da2bdf1b3bd67aa85d23b853733eae7
handriss <andrashinkel@gmail.com>
2017-03-19 02:58:58 +0100

    hello world application created

--- src/main/java/com/codecool/BootBaseApplication.java
+++ src/main/java/com/codecool/BootBaseApplication.java
@@ -2,11 +2,21 @@ package com.codecool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class BootBaseApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BootBaseApplication.class, args);
-	}
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello, Heroku!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootBaseApplication.class, args);
+    }
 }
