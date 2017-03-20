b735d186fc95f6344b07b6d9630ee42fb8ce0aad
handriss <andrashinkel@gmail.com>
2017-03-20 16:54:16 +0100

    deleting now works from modal

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -83,15 +83,17 @@ $(function() {
 $('#confirm-delete').on('click', '.btn-ok', function(e) {
     var $modalDiv = $(e.delegateTarget);
     var id = $(this).data('recordId');
-    // $.ajax({url: '/api/record/' + id, type: 'DELETE'})
-    // $.post('/api/record/' + id).then()
+
     $modalDiv.addClass('loading');
     setTimeout(function() {
         $modalDiv.modal('hide').removeClass('loading');
     }, 1000)
 });
 $('#confirm-delete').on('show.bs.modal', function(e) {
-    var data = $(e.relatedTarget).data();
-    $('.title', this).text('/admin/delete-post/' + data.recordTitle);
-    $('.btn-ok', this).data('recordId', data.recordId);
+
+    var postNumber = $(e.relatedTarget).data().recordTitle;
+
+    $('#deleteButton').attr("href", "/admin/delete-post/" + postNumber);
+
+
 });
