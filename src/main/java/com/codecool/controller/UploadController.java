908f6de66088af6b783d370dc391ae0e6d6ea98d
handriss <andrashinkel@gmail.com>
2017-03-25 17:21:59 +0100

    uploading works programatically2

--- src/main/java/com/codecool/controller/UploadController.java
+++ src/main/java/com/codecool/controller/UploadController.java
@@ -1,11 +1,23 @@
 package com.codecool.controller;
 
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.UUID;
 
 @Slf4j
 @Controller
@@ -21,7 +33,35 @@ public class UploadController {
     }
 
     @PostMapping("/upload-file")
-    public String saveFile(){
+    public String saveFile(@RequestParam("name") String name, @RequestParam("file")MultipartFile file, RedirectAttributes redirectAttributes){
+
+        AWSCredentials credentials = new BasicAWSCredentials("", "");
+
+
+        AmazonS3 s3client = new AmazonS3Client(credentials);
+
+        String bucketName = "kfib-" + UUID.randomUUID();
+
+        s3client.createBucket(bucketName);
+
+        ObjectMetadata md = new ObjectMetadata();
+        md.setContentType("application/pdf");
+
+        try{
+            InputStream inputStream = file.getInputStream();
+
+            s3client.putObject(new PutObjectRequest(bucketName, name, inputStream, md).withCannedAcl(CannedAccessControlList.PublicRead));
+
+            S3Object s3Object = s3client.getObject(new GetObjectRequest(bucketName, name));
+
+            log.info("Url of the image:\n");
+            log.info(s3Object.getObjectContent().getHttpRequest().getURI().toString());
+
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+
+
         log.info("upload called with post method...");
 
         return "redirect:/admin/posts";
