9d7f93879fb2a12ab5fb73e7a92880372ff01938
handriss <andrashinkel@gmail.com>
2017-03-29 13:54:28 +0200

    firebase storage initialized2

--- src/main/resources/static/js/upload.js
+++ src/main/resources/static/js/upload.js
@@ -1,5 +1,18 @@
 $('document').ready(function(){
 
-    console.log("műxik");
+    var config = {
+        apiKey: "AIzaSyAQguIPlj06NUO7AL6niuH0DIHx4sBmto0",
+        authDomain: "kfib-d5ded.firebaseapp.com",
+        databaseURL: "https://kfib-d5ded.firebaseio.com",
+        storageBucket: "kfib-d5ded.appspot.com",
+        messagingSenderId: "823339470062"
+    };
+    firebase.initializeApp(config);
+
+    var storage = firebase.storage();
+
+
+
+    console.log("lefutott jupppíí");
 
 });
