ee2a7955c87b5d34ece19cc806c7427f54aea27f
handriss <andrashinkel@gmail.com>
2017-03-30 13:11:52 +0200

    frontend part of file upload working

--- src/main/resources/static/js/upload.js
+++ src/main/resources/static/js/upload.js
@@ -1,146 +1,33 @@
-$('document').ready(function(){
+$(document).ready(function(){
 
-    var config = {
-        apiKey: "AIzaSyAQguIPlj06NUO7AL6niuH0DIHx4sBmto0",
-        authDomain: "kfib-d5ded.firebaseapp.com",
-        databaseURL: "https://kfib-d5ded.firebaseio.com",
-        storageBucket: "kfib-d5ded.appspot.com",
-        messagingSenderId: "823339470062"
-    };
-    firebase.initializeApp(config);
+    var files;
 
-    var storage = firebase.storage();
+    function uploadFiles(event){
 
+        files = event.target.files;
 
+        var data = new FormData();
 
-    console.log("lefutott jupppíí");
-
-
-    $(function () {
-        var ol = $('#upload ol');
-        $('#drop a').click(function () {
-            $(this).parent().find('input').click();
-        });
-        $('body').on('click', '#openUpload', function (e) {
-            e.preventDefault();
-            $('#upload ol').empty();
-            $('#uploadModal').modal('show');
+        $.each(files, function(key, value)
+        {
+            data.append(key, value);
         });
 
+        $('#fileNameViewer').val(this.files[0].name);
 
-        $('#upload').fileupload({
-            messages: {
-                maxFileSize: "File is too big",
-                minFileSize: "File is too small",
-                acceptFileTypes: "Filetype not allowed",
-                maxNumberOfFiles: "Too many files",
-                uploadedBytes: "Uploaded bytes exceed file size",
-                emptyResult: "Empty file upload result"
-            },
-            // Uncomment the following to send cross-domain cookies:
-            //xhrFields: {withCredentials: true},
-            //url: 'AJAX.ashx',
-            url: 'http://jquery-file-upload.appspot.com/',
-            dataType: 'json',
-            disableImageLoad: true,
-            headers: {
-                Accept: "application/json"
-            },
-            accept: 'application/json',
-            maxFileSize: 10000000, //5mb
-            maxNumberOfFiles: 5,
-            sequentialUploads: true,
-            //singleFileUploads: false,
-            //resizeMaxWidth: 1920,
-            //resizeMaxHeight: 1200,
-            //acceptFileTypes: /(.|\/)(gif|jpe?g|png|pdf)$/i,
-            uploadTemplateId: null,
-            downloadTemplateId: null,
-            uploadTemplate: function (o) {
-                var rows = $();
-                $.each(o.files, function (index, file) {
-                    var row = $('<li class="template-upload fade upload-file">' +
-                        '<div class="upload-progress-bar progress" style="width: 0%;"></div>' +
-                        '<div class="upload-file-info">' +
-                        '<div class="filename-col"><span class="filename"></span></div>' +
-                        '<div class="filesize-col"><span class="size"></span></div>' +
-                        '<div class="error-col"><span class="error field-validation-error"></span></div>' +
-                        '<div class="actions-col">' +
-                        '<button class="btn btn-xs btn-danger cancel removeFile" data-toggle="tooltip" data-placement="left" title="" data-original-title="Remove file">' +
-                        '<i class="glyphicon glyphicon-ban-circle"></i> <span></span>' +
-                        '</button> ' +
-                        '<button class="btn btn-success start"><i class="glyphicon glyphicon-upload"></i> <span>Start</span></button>' +
-                        '</div>' +
-                        '</div>' +
-                        '</li>');
-                    row.find('.filename').text(file.name);
-                    row.find('.size').text(o.formatFileSize(file.size));
-                    if (file.error) {
-                        row.find('.error').text(file.error);
-                    }
-                    rows = rows.add(row);
-                });
-                return rows;
-            },
-            downloadTemplate: function (o) {
-                var rows = $();
-                $.each(o.files, function (index, file) {
-                    var row = $('<li class="template-download fade upload-file complete">' +
-                        '<div class="upload-progress-bar progress" style="width: 100%;"></div>' +
-                        '<div class="upload-file-info">' +
-                        '<div class="filename-col"><span class="filename"></span> - <span class="size"></span></div>' +
-                        '<div class="error-col"><span class="error"></span></div>' +
-                        '</div>' +
-                        '</li>');
+        console.log(data);
 
-                    row.find('.size').text(o.formatFileSize(file.size));
-                    if (file.error) {
-                        row.find('.filename').text(file.name);
-                        row.find('.error').text(file.error);
-                        row.removeClass('complete').addClass('error');
-                    } else {
-                        row.find('.filename').text(file.name);
-                    }
-                    rows = rows.add(row);
-                });
-                return rows;
-            }
-        });
 
-        $('#upload').bind('fileuploadprocessalways', function (e, data) {
-            var currentFile = data.files[data.index];
-            if (data.files.error && currentFile.error) {
-                //console.log(currentFile.error);
-                data.context.find(".start").prop('disabled', true);
-                data.context.find('.error').text(currentFile.error);
-                return;
-            }
-        });
+    }
 
-        /*$(document, '.removeFile').on('show.bs.tooltip', function (e) {
-         e.stopPropagation();
-         }).on('hide.bs.tooltip', function (e) {
-         e.stopPropagation();
-         });*/
-
-        $('#upload').bind('fileuploadadd', function (e, data) {
-            setTimeout(function () {
-                $('.removeFile').tooltip();
-            }, 0);
-            //$('.removeFile').tooltip();
-            //console.log('add');
-        })
-            .bind('fileuploadprogress', function (e, data) {
-                var progress = parseInt(data.loaded / data.total * 100, 10);
-                data.context.find('.progress').css('width', progress + '%');
-                //console.log(progress);
-            })
-            .bind('fileuploadfail', function (e, data) {
-                console.log('fail');
-            }).bind('fileuploadstart', function (e) {
-            console.log('start');
-        })
-
-    });
+    $('input[type=file]').on('change', uploadFiles);
 
 });
+
+// $('#fileInput').change(function(){
+//
+//     // // console.log('fileinput changed');
+//     // console.log(this.files[0].name);
+//
+//     $('#fileNameViewer').val(this.files[0].name);
+// });
