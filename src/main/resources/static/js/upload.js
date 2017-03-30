d6c4296e8e7399b0ea05ba35f72b7f8138b80d07
handriss <andrashinkel@gmail.com>
2017-03-31 01:44:24 +0200

    files about to be uploaded now appear in list2

--- src/main/resources/static/js/upload.js
+++ src/main/resources/static/js/upload.js
@@ -7,11 +7,35 @@ function handleFileSelect(evt) {
     evt.preventDefault();
     var file = evt.target.files[0];
 
+    $('#uploadedFiles').append('<a href="#" class="list-group-item list-group-item-success file-list"><span class="badge alert-success pull-right"><span class="glyphicon glyphicon-remove"></span></span>' + file.name + '</a>');
+
     var uploadTask = storageRef.child(file.name).put(file);
-    uploadTask.on('state_changed', function progressHandler(snapshot) {
-        var percent = snapshot.bytesTransferred / snapshot.totalBytes * 100;
-        console.log(percent + "% done");
-    });
+
+    uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, function(snapshot) {
+
+            var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log('Upload is ' + progress + '% done');
+            switch (snapshot.state) {
+                case firebase.storage.TaskState.PAUSED:
+                    console.log('Upload is paused');
+                    break;
+                case firebase.storage.TaskState.RUNNING:
+                    console.log('Upload is running');
+                    break;
+            }
+
+        }, function(error) {
+
+            console.log("ajajajajaj, valami error történt");
+
+        }, function() {
+
+            var downloadURL = uploadTask.snapshot.downloadURL;
+
+            console.log(downloadURL);
+            // $('#uploadedFiles').append('<li>Működőcske linkecske: ' + downloadURL + '</li>');
+
+        });
 
 }
 
