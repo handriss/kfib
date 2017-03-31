f029296966e87402d0cd78b9089ed45d022ac5bc
handriss <andrashinkel@gmail.com>
2017-03-31 21:23:38 +0200

    files now can be deleted from the list

--- src/main/resources/static/js/upload.js
+++ src/main/resources/static/js/upload.js
@@ -7,17 +7,19 @@ function handleFileSelect(evt) {
     evt.preventDefault();
     var file = evt.target.files[0];
 
-    $('#uploadedFiles').append('<a href="#" class="list-group-item list-group-item-success file-list"><span class="badge alert-success pull-right"><span class="glyphicon glyphicon-remove"></span></span>' + file.name + '</a>');
+    // $('#uploadedFiles').append('<a href="#" class="list-group-item list-group-item-success file-list"><span class="badge alert-success pull-right"><span class="glyphicon glyphicon-remove"></span></span>' + file.name + '</a>');
+    $('#uploadedFiles').append('<a href="#"><div class="alert alert-info alert-labeled"><button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">×</span><span class="sr-only">Bezárás</span></button><div class="alert-labeled-row file-list"><p class="alert-body alert-body-right alert-labelled-cell">' + file.name + '</p></div></div></a>');
 
     var uploadTask = storageRef.child(file.name).put(file);
 
     uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, function(snapshot) {
 
-            var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-            $('#uploadedFiles').width(progress + '%');
+        var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        $('#uploadedFiles a:last-child div').width(progress + '%');
+        console.log($('#uploadedFiles a:last-child div'));
 
 
-        }, function(error) {
+    }, function(error) {
 
             console.log("ajajajajaj, valami error történt");
 
