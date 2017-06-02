160f04c60584b6e121f196fe062ed2aec458b122
handriss <andrashinkel@gmail.com>
2017-06-02 16:41:19 +0200

    posts listing added to homecontroller

--- src/main/java/com/codecool/controller/HomeController.java
+++ src/main/java/com/codecool/controller/HomeController.java
@@ -1,14 +1,25 @@
 package com.codecool.controller;
 
 
+import com.codecool.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
+    private PostService postService;
+
+    @Autowired
+    public HomeController(PostService postService) {
+        this.postService = postService;
+    }
+
     @RequestMapping("/")
-    public String home(){
+    public String home(Model model){
+        model.addAttribute("posts", postService.findAll());
         return "index";
     }
 
